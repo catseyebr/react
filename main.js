48bb7d3986920e73c305dbc8d248f6e0d7a2ede8
Eduardo Assis <eduardoassis@outlook.com.br>
2017-12-30 07:57:00 -0200

    Inicial index

---
+++ main.js
@@ -0,0 +1,14 @@
+var Nav = React.createClass({
+    render:function(){
+        return(
+            <nav className="navbar navbar-default">
+
+            </nav>
+        )
+    }
+});
+
+ReactDOM.render(
+<h1>Hello React!</h1>,
+document.getElementById('app')
+);
